f593d6a63e85f85d5a7793ae24249847a248e077
dommeudec <72738163+dommeudec@users.noreply.github.com>
2022-10-25 13:15:12 +0100

    Updated test

--- src/main/java/ie/tcd/meudecd/Index.java
+++ src/main/java/ie/tcd/meudecd/Index.java
@@ -1,24 +1,23 @@
 package ie.tcd.meudecd;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 import java.nio.file.Paths;
-import java.nio.file.Files;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
-
-public class Analysis {
-
+// import org.apache.lucene.store.RAMDirectory;
+ 
+public class Index
+{
+    
     // Directory where the search index will be saved
     private static String INDEX_DIRECTORY = "index";
 
@@ -26,27 +25,27 @@ public class Analysis {
     {
         // Analyzer that is used to process TextField
         Analyzer analyzer = new StandardAnalyzer();
-
+        
         // To store an index in memory
         // Directory directory = new RAMDirectory();
         // To store an index on disk
         Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
+        
         // Index opening mode
         // IndexWriterConfig.OpenMode.CREATE = create a new index
         // IndexWriterConfig.OpenMode.APPEND = open an existing index
         // IndexWriterConfig.OpenMode.CREATE_OR_APPEND = create an index if it
         // does not exist, otherwise it opens it
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-
-        IndexWriter iwriter = new IndexWriter(directory, config);
-
+        
+        IndexWriter iwriter = new IndexWriter(directory, config);  
+        
         // Create a new document
         Document doc = new Document();
-        doc.add(new TextField("super_name", "Spider-MAN1", Field.Store.YES));
-        doc.add(new TextField("name", "Peter ParkER1", Field.Store.YES));
-        doc.add(new TextField("category", "superheRO0", Field.Store.YES));
+        doc.add(new TextField("super_name", "Goal", Field.Store.YES));
+        doc.add(new TextField("name", "Dom Meudec", Field.Store.YES));
+        doc.add(new TextField("category", "football", Field.Store.YES));
 
         // Save the document to the index
         iwriter.addDocument(doc);
@@ -55,5 +54,4 @@ public class Analysis {
         iwriter.close();
         directory.close();
     }
-
 }
